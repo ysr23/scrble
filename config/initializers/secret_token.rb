9a4b3612890379683393c797b487fedf0c733f58
thomas mceldowney <toast@manchester.com>
2013-11-29 23:44:39 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scrabble::Application.config.secret_token = '89675fae29d8d9d2689d6d1994e39d15a2bffae26d1a947a882af64f3cd2953dc90980c363fe47c54fab5376f6bba7e8c4cf14614bccfe0bac8f6ae9b847c576'
